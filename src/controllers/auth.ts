3db0031b979bb3f5100493ac0a43f91c4c78099b
Ám Vương <102019281+MeoDaoSi@users.noreply.github.com>
2024-04-08 09:24:38 +0700

    add auth routes and mw

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -9,11 +9,20 @@ export const register = asyncHandler(async (req: Request, res: Response) => {
     const user = await UserModel.findOne({ email: req.body.email });
     if (user) throw new Error('User already exists');
     const passwordHash = await Bcrypt.hash(req.body.password, 10);
-    const newUser = await UserModel.create({ email: req.body.email, password: passwordHash });
+    const newUser = await UserModel.create({ full_name: req.body.full_name, email: req.body.email, password: passwordHash });
     console.log(process.env.JWT_SECRET_KEY);
 
     const token = await jwt.sign({ _id: newUser._id }, process.env.JWT_SECRET_KEY!);
     res.status(201).json({ newUser, token });
+})
+
+export const login = asyncHandler(async (req: Request, res: Response) => {
+    const user = await UserModel.findOne({ email: req.body.email });
+    if (!user) throw new Error('User not found');
+    const isValid = await Bcrypt.compare(req.body.password, user.password!);
+    if (!isValid) throw new Error('Invalid password');
+    const token = await jwt.sign({ _id: user._id }, process.env.JWT_SECRET_KEY!);
+    res.status(200).json({ user, token });
 })
 
 export const getAll = asyncHandler(async (req: Request, res: Response) => {
--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-import { register, get } from '../controllers/auth';
+import { register, get, login } from '../controllers/auth';
 import { auth } from '../middlewares/auth';
 
 const router = express.Router();
 
 router.post('/register', register);
+router.post('/login', login);
 
 router.use(auth);
 
